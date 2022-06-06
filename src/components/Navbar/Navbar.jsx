0092666adebb01feb29dc6d25dde53ed0723f99d
manishtmtmt <manisht.mt.mt@gmail.com>
2022-06-06 20:50:46 +0530

    updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 
 // use react-router Link or NavLink
 
 const Navbar = () => {
-  const {isAuth, logout} = useContext(AuthContext)
+  const { isAuth, logout } = useContext(AuthContext);
   const navigate = useNavigate();
+  const [cart, setCart] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:8080/cartItems")
+      .then((r) => r.json())
+      .then((res) => {
+        setCart(res);
+      });
+  }, []);
 
   const handleOnClick = () => {
-    if(isAuth) {
+    if (isAuth) {
       logout();
+    } else {
+      navigate("/login");
     }
-    else {
-      navigate("/login")
-    }
-  }
+  };
 
   return (
     <div data-cy="navbar">
-      <Link to="/" data-cy="navbar-home-link">Logo</Link>
-      <span data-cy="navbar-cart-items-count">{/* count here */}</span>
-      <button onClick={handleOnClick} data-cy="navbar-login-logout-button">{
-        isAuth ? "Logout" : "Login"
-      }</button>
+      <Link to="/" data-cy="navbar-home-link">
+        Logo
+      </Link>
+      <span data-cy="navbar-cart-items-count">
+        Cart: 
+        {isAuth ? cart.length : "0"}
+      </span>
+      <button onClick={handleOnClick} data-cy="navbar-login-logout-button">
+        {isAuth ? "Logout" : "Login"}
+      </button>
     </div>
   );
 };
